61e2a82fb97f5e97a3c8a39817698046f35c1548
ludoc <ludoc1012@outlook.com>
2025-02-08 14:15:41 +0800

    Remove unused files and dependencies from the project

--- Chatbot/src/App.jsx
+++ Chatbot/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import { ChatInputComponent } from './components/ChatInputComponent';
+import { ChatMessageComponent } from './components/ChatMessage';
 import './App.css'
 
+
+
+
 function App() {
-  const [count, setCoun] = useState(0)
+  const [chatMessages, setChatMessages] = useState(
+    [
+      {
+        message: 'hello chatbot',
+        sender: 'user',
+        key: 'id1'
+      },
+      {
+        message: 'Hello! How can I help you?',
+        sender: 'robot',
+        key: 'id2'
+      }
+    ]
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="chatbot">
+      <ChatMessageComponent
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />
+      <ChatInputComponent
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />
+    </div>
+  );
+};
 
 export default App
